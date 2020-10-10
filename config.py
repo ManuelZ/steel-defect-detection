819da47d6ee22cda901c7eb7167a8c5ecc92ad96
Manuel Zanutelli <mclzc89@gmail.com>
2020-10-10 15:07:51 -0500

    clean config file

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 
+# Built-in imports
 from pathlib import Path
+
+# External imports
 from mrcnn.config import Config
 
+
+# Directory where the data and pre-trained weight files are located
 ROOT_DIR = Path('E:\Datasets\steel-defects-detection')
 
 # Directory to save logs and trained model
@@ -11,19 +16,27 @@ MODEL_DIR = ROOT_DIR / 'models'
 COCO_MODEL_PATH = ROOT_DIR / "mask_rcnn_coco.h5"
 
 # Path towards the directory with the train images
-TRAIN_IMAGES_DIR = ROOT_DIR/'train_images'
+TRAIN_IMAGES_DIR = ROOT_DIR / 'train_images'
 
 # Path towards the directory with the test images
-TEST_IMAGES_DIR = ROOT_DIR/'test_images'
+TEST_IMAGES_DIR = ROOT_DIR / 'test_images'
 
-CSV_FILE = ROOT_DIR/'train.csv'
+# CSV file with all the training masks and information
+CSV_FILE = ROOT_DIR / 'train.csv'
 
 
 class SteelConfig(Config):
     """
+    Standard way of configuring Mask-RCNN: subclass Config.
     """
+
+    # A name is required
     NAME = 'steel'
-    BACKBONE = 'resnet50'
+    
+    # Either 'resnet50' or 'resnet101'
+    BACKBONE = 'resnet101'
+    
+    # For GPUs with not so much memory, use 1
     IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
